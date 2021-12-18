341ddc36fb9dcf4e0c0fae1c0b3b38204927b283
Vitali Baumtrok <vbsw@mailbox.org>
2021-12-18 21:57:07 +0100

    notes added

--- OSArgs.cpp
+++ OSArgs.cpp
@@ -1,3 +1,10 @@
+/*
+ *          Copyright 2021, Vitali Baumtrok.
+ * Distributed under the Boost Software License, Version 1.0.
+ *     (See accompanying file LICENSE or copy at
+ *        http://www.boost.org/LICENSE_1_0.txt)
+ */
+
 #include "OSArgs.h"
 #include "g2d/String.h"
 
